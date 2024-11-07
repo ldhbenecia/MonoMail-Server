71188cf90a6e65028dc978f64678d802cbae99cd
DongHyeok Lim <jh07050@gmail.com>
2024-11-07 09:31:17 +0900

    Fix: Pin 제거 시 Body가 아닌 PathVariable 사용

--- src/main/java/woozlabs/echo/domain/contact/controller/PinController.java
+++ src/main/java/woozlabs/echo/domain/contact/controller/PinController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,10 +34,10 @@ public class PinController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/pin")
-    public ResponseEntity<Void> deletePin(@RequestParam("aAUid") String activeAccountUid,
-                                          @RequestBody PinRequestDto requestDto) {
-        pinService.deletePin(activeAccountUid, requestDto.getPinnedEmail());
+    @DeleteMapping("/pin/{pinnedEmail}")
+    public ResponseEntity<Void> deletePin(@PathVariable("pinnedEmail") String pinnedEmail,
+                                          @RequestParam("aAUid") String activeAccountUid) {
+        pinService.deletePin(activeAccountUid, pinnedEmail);
         return ResponseEntity.noContent().build();
     }
 }
