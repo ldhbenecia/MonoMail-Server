263dbcc05cd7addd8f382aaf11e55efb45c41503
Kwanwoo Kim <kim.kw8012@gmail.com>
2024-11-08 14:17:51 -0800

    fix: commented account signature

--- src/main/java/woozlabs/echo/domain/member/service/AccountService.java
+++ src/main/java/woozlabs/echo/domain/member/service/AccountService.java
@@ -74,7 +74,7 @@ public class AccountService {
                                 .provider(account.getProvider())
                                 .isExpired(account.getAccessToken() == null)
                                 .scopes(grantedScopes)
-                                .defaultSignatureId(account.getDefaultSignature().getId())
+                                // .defaultSignatureId(account.getDefaultSignature().getId())
                                 .build();
                     })
                     .collect(Collectors.toList());
@@ -107,7 +107,7 @@ public class AccountService {
                     .provider(currentAccount.getProvider())
                     .isExpired(currentAccount.getAccessToken() == null)
                     .scopes(googleOAuthUtils.getGrantedScopes(currentAccount.getAccessToken()))
-                    .defaultSignatureId(currentAccount.getDefaultSignature().getId())
+                //     .defaultSignatureId(currentAccount.getDefaultSignature().getId())
                     .build();
 
             List<GetAccountResponseDto.RelatedMemberDto> relatedMembers = memberAccounts.stream()
