e52df430894e144b61768acfd4a04125c3953ba7
DongHyeok Lim <jh07050@gmail.com>
2024-11-08 21:34:04 +0900

    Feat: Preference Entity 분리 설계

--- src/main/java/woozlabs/echo/domain/member/entity/Member.java
+++ src/main/java/woozlabs/echo/domain/member/entity/Member.java
@@ -1,31 +1,22 @@
 package woozlabs.echo.domain.member.entity;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapKeyColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 import woozlabs.echo.global.common.entity.BaseEntity;
 
 @Entity
@@ -48,41 +39,13 @@ public class Member extends BaseEntity {
     private String memberName;
     private String profileImageUrl;
     private String primaryUid;
-    private String language = "en";
-
-    @Enumerated(EnumType.STRING)
-    private Theme theme = Theme.LIGHT;
-
-    private boolean marketingEmails;
     private LocalDateTime deletedAt;
 
-    @ColumnDefault("true")
-    private boolean securityEmails = true;
-
-    @Enumerated(EnumType.STRING)
-    private Density density = Density.COMPACT;
+    @OneToOne(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private MemberPreference preference;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MemberAccount> memberAccounts = new ArrayList<>();
-
-    @ElementCollection
-    @CollectionTable(name = "accounts_watch_notifications", joinColumns = @JoinColumn(name = "member_id"))
-    @MapKeyColumn(name = "account_uid")
-    @Column(name = "notification_preference")
-    @Enumerated(EnumType.STRING)
-    private Map<String, Watch> watchNotifications = new HashMap<>();
-
-    @ElementCollection
-    @CollectionTable(name = "member_term_agreements", joinColumns = @JoinColumn(name = "member_id"))
-    @MapKeyColumn(name = "agreement_type")
-    @Column(name = "timestamp")
-    private Map<String, LocalDateTime> termAgreements = new HashMap<>();
-
-    @ElementCollection
-    @CollectionTable(name = "member_marketing_agreements", joinColumns = @JoinColumn(name = "member_id"))
-    @MapKeyColumn(name = "agreement_type")
-    @Column(name = "timestamp")
-    private Map<String, LocalDateTime> marketingAgreements = new HashMap<>();
 
     public void addMemberAccount(MemberAccount memberAccount) {
         this.memberAccounts.add(memberAccount);
