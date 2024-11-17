64b21cb7f16c1aac2829a2a97810f67c4eaa97ea
DongHyeok Lim <jh07050@gmail.com>
2024-11-17 19:24:44 +0900

    Refactor: resolveFeedback PUT -> PATCH로 수정

--- src/main/java/woozlabs/echo/domain/feedback/controller/FeedbackController.java
+++ src/main/java/woozlabs/echo/domain/feedback/controller/FeedbackController.java
@@ -4,9 +4,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +31,7 @@ public class FeedbackController {
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/{feedbackId}/resolve")
+    @PatchMapping("/{feedbackId}/resolve")
     public ResponseEntity<Void> resolveFeedback(@PathVariable("feedbackId") String feedbackId) {
         feedbackService.resolvedFeedback(feedbackId);
         return ResponseEntity.ok().build();
